5c377d697f74af7c3b3b7ab89045e235ca99ed25
SmallBoyO <554645932@qq.com>
2018-06-28 00:53:22 +0800

    testService添加延迟10毫秒

--- src/main/java/com/zhanghe/proxy/MessageCallBack.java
+++ src/main/java/com/zhanghe/proxy/MessageCallBack.java
@@ -26,7 +26,7 @@ public class MessageCallBack {
 			if(response!=null){
 				return response.result;
 			}else{
-				//超时时间设置为10秒
+				//超时时间设置为10秒 调用Condition的await()方法，会使当前线程进入等待队列并释放锁，同时线程状态变为等待状态。
 				condition.await(11*1000,TimeUnit.MILLISECONDS);
 				if(response!=null){
 					return response.result;
@@ -42,8 +42,10 @@ public class MessageCallBack {
 	public void over(RpcResponse reponse) {
         try {
             lock.lock();
-            condition.signalAll();
             this.response = reponse;
+			//调用Condition的signal()方法，将会唤醒在等待队列中等待时间最长的节点（首节点），在唤醒节点之前，会将节点移到同步队列中。
+			//Condition的signalAll()方法，相当于对等待队列中的每个节点均执行一次signal()方法，将等待队列中的节点全部移动到同步队列中，并唤醒每个节点的线程。
+			condition.signalAll();
         } finally {
             lock.unlock();
         }
